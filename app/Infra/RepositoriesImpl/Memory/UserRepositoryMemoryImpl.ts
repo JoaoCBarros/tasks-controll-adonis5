ae594ec8c1dfaff4a418e861a4a416b2143c46a9
joao.pedro6 <joao.pedro@ubistart.com>
2022-08-16 08:59:46 -0300

    Created basic structure to User Entity

---
+++ app/Infra/RepositoriesImpl/Memory/UserRepositoryMemoryImpl.ts
@@ -0,0 +1,37 @@
+import CreateUserInput from 'App/modules/User/dto/CreateUserInput'
+import UserEntity from 'App/modules/User/Entity/UserEntity'
+import UserRepository from 'App/Repositories/UserRepository'
+
+export default class UserRepositoryMemoryImpl implements UserRepository {
+  private users: UserEntity[] = []
+
+  public async getUserById(userId: number): Promise<UserEntity> {
+    const usersFilteredById = this.users.filter((user) => {
+      return user.id === userId
+    })
+
+    this.haveAnyUser(usersFilteredById)
+
+    return usersFilteredById[0]
+  }
+  public async createUser(input: CreateUserInput): Promise<UserEntity> {
+    const newUserId = this.users.length + 1
+
+    this.users.push({
+      ...input,
+      id: newUserId,
+    })
+
+    return await this.getUserById(newUserId)
+  }
+
+  private haveAnyUser(users: UserEntity[]) {
+    if (users.length === 0) {
+      throw new Error('USER_NOT_FOUND')
+    }
+  }
+
+  public async getUserByEmail(email: string): Promise<UserEntity> {
+    throw new Error('Method not implemented.')
+  }
+}
--- app/Repositories/UserRepository.ts
+++ app/Repositories/UserRepository.ts
@@ -1,5 +1,8 @@
+import CreateUserInput from 'App/modules/User/dto/CreateUserInput'
 import UserEntity from 'App/modules/User/Entity/UserEntity'
 
 export default interface UserRepository {
   getUserById(userId: number): Promise<UserEntity>
+  createUser(input: CreateUserInput): Promise<UserEntity>
+  getUserByEmail(email: string): Promise<UserEntity>
 }
