2531aa3e04dcb51306a4661618b4552197a11ce7
joao.pedro6 <joao.pedro@ubistart.com>
2022-08-22 08:17:17 -0300

    Created Login and Loggout Session

---
+++ start/session/session-routes.ts
@@ -0,0 +1,4 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.post('login', 'SessionsController.createSession').namespace('App/Infra/Http/Session')
+Route.post('loggout', 'SessionsController.destroySession').namespace('App/Infra/Http/Session')
